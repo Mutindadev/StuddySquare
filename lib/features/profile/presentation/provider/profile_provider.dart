41532a3c5c3a085e557c12de6a3275f7ca9cc0fa
eltopu <eltonmwangi8@gmail.com>
2025-11-02 19:45:50 +0300

    small fix in profile provider

--- lib/features/profile/presentation/provider/profile_provider.dart
+++ lib/features/profile/presentation/provider/profile_provider.dart
@@ -1,7 +1,6 @@
 // ...existing code...
 import 'dart:convert';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:studysquare/features/auth/presentation/provider/auth_provider.dart';
